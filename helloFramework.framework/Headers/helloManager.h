7a343b5467f661a937571eec6e8d2cb63aedd9ca
suxiangxiao <13751882497@163.com>
2016-06-06 02:06:49 +0800

    sdf

---
+++ helloFramework.framework/Headers/helloManager.h
@@ -0,0 +1,15 @@
+//
+//  helloManager.h
+//  helloFramework
+//
+//  Created by suxx on 16/6/5.
+//  Copyright © 2016年 suxx. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface helloManager : NSObject
+
+-(void)sayHello;
+
+@end
