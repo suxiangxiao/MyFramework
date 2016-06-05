187b6741df4ac58723a4dbb3deb54b6f0bcb3b7c
suxiangxiao <13751882497@163.com>
2016-06-06 01:18:43 +0800

    MyFramework init

---
+++ helloFramework/helloFramework.h
@@ -0,0 +1,20 @@
+//
+//  helloFramework.h
+//  helloFramework
+//
+//  Created by suxx on 16/6/5.
+//  Copyright © 2016年 suxx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for helloFramework.
+FOUNDATION_EXPORT double helloFrameworkVersionNumber;
+
+//! Project version string for helloFramework.
+FOUNDATION_EXPORT const unsigned char helloFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <helloFramework/PublicHeader.h>
+#import <helloFramework/helloManager.h>
+
+
---
+++ helloFramework/helloManager.m
@@ -0,0 +1,20 @@
+//
+//  helloManager.m
+//  helloFramework
+//
+//  Created by suxx on 16/6/5.
+//  Copyright © 2016年 suxx. All rights reserved.
+//
+
+#import "helloManager.h"
+
+@implementation helloManager
+
+#pragma mark - Private Method
+
+#pragma mark - Public Method
+-(void)sayHello{
+    NSLog(@"hello world");
+}
+
+@end
